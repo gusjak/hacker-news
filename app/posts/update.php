640dd6d48b6c4d0aacafea2d3d3f9963bce90215
gusjak <70512255+gusjak@users.noreply.github.com>
2021-01-07 20:46:47 +0100

    Fixed Lint error

--- app/posts/update.php
+++ app/posts/update.php
@@ -4,28 +4,62 @@ declare(strict_types=1);
 
 require __DIR__ . '/../autoload.php';
 
-if (loggedIn()) {
-    $newTitle = trim(filter_var($_POST['edit-title']));
-    $newUrl = trim(filter_var($_POST['edit-url'], FILTER_SANITIZE_STRING));
-    $newText = trim(filter_var($_POST['edit-text-content'], FILTER_SANITIZE_STRING));
-
+if (loggedIn() && isset($_POST['edit-title'], $_POST['edit-url'], $_POST['edit-text-content'])) {
     $id = (int) $_SESSION['user']['id'];
-    $username = $_SESSION['user']['username'];
+    $newTitle = trim(filter_var($_POST['edit-title'], FILTER_SANITIZE_STRING));
+    $newUrl = trim(filter_var($_POST['edit-url'], FILTER_SANITIZE_URL));
+    $newTextContent = trim(filter_var($_POST['edit-text-content'], FILTER_SANITIZE_STRING));
 
-    $statement = $pdo->prepare('UPDATE posts SET title = :title, url = :url, text = :text WHERE id = :id AND user_id = :user_id');
+    // Save new title changes to the database.
+    if ($_POST['edit-title'] == '') {
+        $_SESSION['message'] = 'No changes made to title';
+    } else {
+        $statement = $pdo->prepare('UPDATE posts SET title = :title WHERE id = :id AND user_id = :user_id');
 
-    if (!$statement) {
-        die(var_dump($pdo->errorInfo()));
+        if (!$statement) {
+            die(var_dump($pdo->errorInfo()));
+        }
+
+        $statement->bindParam(':title', $newTitle, PDO::PARAM_STR);
+        $statement->bindParam(':id', $id, PDO::PARAM_INT);
+        $statement->execute();
+
+        $_SESSION['message'] = 'Your settings has been updated.';
     }
 
-    $statement->bindParam(':title', $newTitle, PDO::PARAM_STR);
-    $statement->bindParam(':url', $newUrl, PDO::PARAM_STR);
-    $statement->bindParam(':text', $newText, PDO::PARAM_STR);
-    $statement->bindParam(':user_id', $id, PDO::PARAM_INT);
+    // Save new url changes to the database.
+    if ($_POST['edit-url'] == '') {
+        $_SESSION['message'] = 'No changes made to URL.';
+    } else {
+        $statement = $pdo->prepare('UPDATE posts SET url = :url WHERE id = :id AND user_id = :user_id');
 
-    $statement->execute();
+        if (!$statement) {
+            die(var_dump($pdo->errorInfo()));
+        }
 
-    $_SESSION['message'] = 'Your post has been updated.';
+        $statement->bindParam(':url', $newUrl, PDO::PARAM_STR);
+        $statement->bindParam(':id', $id, PDO::PARAM_INT);
+        $statement->execute();
+
+        $_SESSION['message'] = 'Your settings has been updated.';
+    }
+
+    // Save text content changes to the database.
+    if ($_POST['edit-text-content'] == '') {
+        $_SESSION['message'] = 'No changes made to text.';
+    } else {
+        $statement = $pdo->prepare('UPDATE posts SET text = :text WHERE id = :id AND user_id = :user_id');
+
+        if (!$statement) {
+            die(var_dump($pdo->errorInfo()));
+        }
+
+        $statement->bindParam(':text', $newText, PDO::PARAM_STR);
+        $statement->bindParam(':id', $id, PDO::PARAM_INT);
+        $statement->execute();
+
+        $_SESSION['message'] = 'Your personal settings has been updated';
+    }
+
+    redirect('/posts.php');
 }
-
-redirect('/posts.php');
--- posts.php
+++ posts.php
@@ -10,40 +10,40 @@ $posts = displayUserPosts($id, $pdo);
 <article>
     <p><?php $message ?></p>
 
-    <h4><?= $user['username'] ?>'s posts.</h4>
+    <h4><?= $user['username'] ?>'s submitted posts.</h4>
 
-    <article>
-        <?php foreach ($posts as $post) : ?>
-            <?php $currentUserId = $_SESSION['user']['id']; ?>
-            <?php $postId = $post['user_id']; ?>
+    <?php foreach ($posts as $post) : ?>
+        <?php $currentUserId = $_SESSION['user']['id']; ?>
+        <?php $postId = $post['user_id']; ?>
+        <br>
+        <form action="app/posts/update.php" method="post" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="edit-title"><?php echo $post['title'] ?></label>
+                <input class="form-control" type="text" name="edit-title" id="edit-title">
+                <small class=" form-text text-muted">You may change the title by filling out this field.</small>
+            </div><!-- /form-group -->
+
+            <div class="form-group">
+                <label for="edit-url"><a href="<?php echo $post['url'] ?>"><?php echo $post['url'] ?></a></label>
+                <input class="form-control" type="url" name="edit-url" id="edit-url">
+                <small class=" form-text text-muted">You may change the url by filling out this field.</small>
+            </div><!-- /form-group -->
+
+            <div class="form-group">
+                <label for="edit-text-content">Text Content</label>
+                <textarea class="form-control rounded-0" name="edit-text-content" id="edit-text-content" rows="5" placeholder="<?= $post['text'] ?>"></textarea>
+            </div><!-- /form-group -->
+
+            <small class="form-text text-muted">Posted: <?php echo $post['date'] . "\n"; ?>
+                <a href="">Comment</a>
+            </small>
             <br>
-            <form action="app/posts/update.php" method="post" enctype="multipart/form-data">
-                <div class="form-group">
-                    <label for="title"><?php echo $post['title'] ?></label>
-                    <input class="form-control" type="text" name="edit-title">
-                    <small class=" form-text text-muted">You may change the title by filling out this field.</small>
-                </div><!-- /form-group -->
 
-                <div class="form-group">
-                    <label for="url"><a href="<?php echo $post['url'] ?>"><?php echo $post['url'] ?></a></label>
-                    <input class="form-control" type="url" name="edit-url">
-                    <small class=" form-text text-muted">You may change the url by filling out this field.</small>
-                </div><!-- /form-group -->
+            <button type="submit" class="btn btn-secondary">Edit post</button>
+            <button type="button" class="btn btn-danger">Delete post</button>
+        </form>
+        <br>
+    <?php endforeach; ?>
+</article>
 
-                <div class="form-group">
-                    <label for="text">Text Content</label>
-                    <textarea class="form-control rounded-0" name="edit-text-content" rows="5" placeholder="<?= $post['text'] ?>"></textarea>
-                </div><!-- /form-group -->
-
-                <small class="form-text text-muted">Posted: <?php echo $post['date'] . "\n"; ?>
-                    <a href="">Comment</a>
-                </small>
-                <br>
-
-                <button type="submit" class="btn btn-secondary">Edit post</button>
-                <button type="button" class="btn btn-danger">Delete post</button>
-                <br>
-            <?php endforeach; ?>
-    </article>
-
-    <?php require __DIR__ . '/views/footer.php'; ?>
+<?php require __DIR__ . '/views/footer.php'; ?>
